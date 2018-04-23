e6a0bdf554ee9a6c2f8bc4d79190006f93fcb4d6
Krystian Kujawski <kujawski.krystian@gmail.com>
2018-04-23 15:01:07 +0200

    Added first excersise form python and most of second

---
+++ python_lvl_one/Part9_Functions_Exercises.py
@@ -0,0 +1,152 @@
+#####################################
+#### PART 9: FUNCTION EXERCISES #####
+#####################################
+
+
+# Complete the tasks below by writing functions! Keep in mind, these can be
+# really tough, its all about breaking the problem down into smaller, logical
+# steps. If you get stuck, don't feel bad about having to peek to the solutions!
+
+#####################
+## -- PROBLEM 1 -- ##
+#####################
+
+# Given a list of integers, return True if the sequence of numbers 1, 2, 3
+# appears in the list somewhere.
+
+# For example:
+
+# arrayCheck([1, 1, 2, 3, 1]) → True
+# arrayCheck([1, 1, 2, 4, 1]) → False
+# arrayCheck([1, 1, 2, 1, 2, 3]) → True
+
+def arrayCheck(nums):
+    # CODE GOES HERE
+
+    isone=0
+    istwo=0
+    isthree=0
+    for i in range(len(nums)):
+        if nums[i]==1:
+            isone += 1
+        if nums[i]==2:
+            istwo += 1
+        if nums[i]==3:
+            isthree += 1
+    if isone >0 and istwo>0 and isthree>0:
+        return True
+    else: return False
+
+
+#####################
+## -- PROBLEM 2 -- ##
+#####################
+
+# Given a string, return a new string made of every other character starting
+# with the first, so "Hello" yields "Hlo".
+
+# For example:
+
+# stringBits('Hello') → 'Hlo'
+# stringBits('Hi') → 'H'
+# stringBits('Heeololeo') → 'Hello'
+
+def stringBits(str):
+  # CODE GOES HERE
+  nstr=''
+  for i in range (0, len(str), 2):
+      nstr+=str[i]
+  return nstr
+
+
+#####################
+## -- PROBLEM 3 -- ##
+#####################
+
+# Given two strings, return True if either of the strings appears at the very end
+# of the other string, ignoring upper/lower case differences (in other words, the
+# computation should not be "case sensitive").
+#
+# Note: s.lower() returns the lowercase version of a string.
+#
+# Examples:
+#
+# end_other('Hiabc', 'abc') → True
+# end_other('AbC', 'HiaBc') → True
+# end_other('abc', 'abXabc') → True
+
+
+def end_other(a, b):
+    a=a.lower()
+    b=b.lower()
+    if len(a)<=len(b):
+        for i in (0,-len(a),-1):
+            if a[i]!=b[i+(len(b)-len(a))]:
+                return False
+            return True
+    else:
+        for i in (0,-len(b),-1):
+            if a[i+(len(a)-len(b))]!=b[i]:
+                return False
+            return True
+
+
+#####################
+## -- PROBLEM 4 -- ##
+#####################
+
+# Given a string, return a string where for every char in the original,
+# there are two chars.
+
+# doubleChar('The') → 'TThhee'
+# doubleChar('AAbb') → 'AAAAbbbb'
+# doubleChar('Hi-There') → 'HHii--TThheerree'
+
+def doubleChar(str):
+    nstr=""
+    for letter in str :
+        nstr+= letter + letter
+    return nstr
+
+print(doubleChar('Hi-There'))
+
+#####################
+## -- PROBLEM 5 -- ##
+#####################
+
+# Read this problem statement carefully!
+
+# Given 3 int values, a b c, return their sum. However, if any of the values is a
+# teen -- in the range 13-19 inclusive -- then that value counts as 0, except 15
+# and 16 do not count as a teens. Write a separate helper "def fix_teen(n):"that
+# takes in an int value and returns that value fixed for the teen rule.
+#
+# In this way, you avoid repeating the teen code 3 times (i.e. "decomposition").
+# Define the helper below and at the same indent level as the main no_teen_sum().
+# Again, you will have two functions for this problem!
+#
+# Examples:
+#
+# no_teen_sum(1, 2, 3) → 6
+# no_teen_sum(2, 13, 1) → 3
+# no_teen_sum(2, 1, 14) → 3
+
+#def no_teen_sum(a, b, c):
+  # CODE GOES HERE
+#def fix_teen(n):
+  # CODE GOES HERE
+
+#####################
+## -- PROBLEM 6 -- ##
+#####################
+
+# Return the number of even integers in the given array.
+#
+# Examples:
+#
+# count_evens([2, 1, 2, 3, 4]) → 3
+# count_evens([2, 2, 0]) → 3
+# count_evens([1, 3, 5]) → 0
+
+#def count_evens(nums):
+  # CODE GOES HERE
---
+++ python_lvl_one/notes.py
@@ -0,0 +1,41 @@
+#dictionary
+
+my_stuff = {"key1":"value","key2":"value2",'key3':{'123':[1,2,"grabMe"]}}
+#print(my_stuff['key3']["123"][2])
+
+
+my_stuff={'lunch':'pizza','bfast':'eggs'}
+my_stuff['lunch']='burger'
+#
+# print(my_stuff)
+
+
+#BooLeands
+
+
+True
+False
+0
+1
+
+
+# Tuples
+t = (1,2,3)
+print(t[0])
+
+t=('a', True, 123)
+
+#t[0] = 'NEW' - error
+
+
+#SETS
+
+x=set()
+
+x.add(1)
+x.add(2)
+x.add(5)
+x.add(5)
+x.add(5)
+
+print(x)
